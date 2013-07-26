e599fc8166ba0c4d16a87a30130c79043de480a6
kang hwan ki <xangfi@gmail.com>
2013-07-26 18:10:36 +0900

    정리중.

--- methodzero.js
+++ methodzero.js
@@ -3,48 +3,70 @@
 //	2013.07.15.
 //	kang hwan ki
 //
-;(function(){ "use strict";
+;(function(){
 
+"use strict";
 var root 	= this;
-var mz 		= root.mz = {};
+var mz 		= {};
+var _mz_ 	= _mz_ || {};
 
-mz.VERSION = "0.0.0.1";
-mz.core = {};
-mz.util = {};
-mz.doc = {};
-
-mz.core.BREAK = {};
-
-// --------------------------------------------------
-//	logger
-// --------------------------------------------------
-
-mz.debug = function(){
-	var arg = arguments;
-	console.groupCollapsed("[DEBUG]"+ _mz_.now(" MM월DD일 HH시MI분"));
-	mz.core.loop(arg,function(c,i){
-		console.log(c);
-	});
-	console.groupEnd();
+// -------------------------------------------------
+//	private
+//	namespace : '_mz_'
+// -------------------------------------------------
+_mz_.toString = function(o) {
+	return Object.prototype.toString.call(o);
 };
 
+_mz_.now = function(fmt) {
+	var dt 	= new Date();
+	fmt 	= fmt || "YYYY/MM/DD HH:MI:SS:MS";
+	return fmt.replace("YYYY",dt.getFullYear())
+	.replace("YY",dt.getFullYear().toString().substring(2))
+	.replace("MM",(dt.getMonth() + 1))
+	.replace("DD",dt.getDate())
+	.replace("HH",dt.getHours())
+	.replace("MI",dt.getMinutes())
+	.replace("SS",dt.getSeconds())
+	.replace("MS",dt.getMilliseconds());
+};
+
+_mz_.document = function() {
+	return _mz_.doc = _mz_.doc || (window ? window.document : undefined);
+};
+
+mz.VERSION = "0.0.0.1";
+
+// log4js
+mz.log = {level:0,DEBUG:1,INFO:2,WARN:3,ERROR:4,FATAL:5};
+mz.log.level = function(level){};
+mz.log.debug = function(){
+	var arg = arguments;
+	console.groupCollapsed("[DEBUG]"+ _mz_.now(" MM월DD일 HH시MI분"));
+	mz.core.each(
+		arg,
+		function(c,i){
+			console.log(c);
+		}
+	);
+	console.groupEnd();
+};
 
 // --------------------------------------------------
 //	core
 // --------------------------------------------------
+mz.core = {};
+mz.core.BREAK = {};
 
 //
-//
-mz.core.loop = function(c, f, context) {
-	if(c.length === c.length+0) {
-		for(var i=0,l=c.length; i<l; i++){
-			if (f.call(context, c[i], i, c) === mz.core.BREAK)
-				return;
+mz.core.each = function(collection, filter, context) {
+	if(collection.length === collection.length+0) {
+		for(var i=0,l=collection.length; i<l; i++){
+			if (filter.call(context, collection[i], i, collection) === mz.core.BREAK) return;
 		}
 	} else {
-		for(var key in c) {
-			if (f.call(context, c[key], key, c) === mz.core.BREAK)
-				return;
+		for(var key in collection) {
+			if (filter.call(context, collection[key], key, collection) === mz.core.BREAK) return;
 		}
 	}
 };
@@ -52,16 +74,14 @@ mz.core.loop = function(c, f, context) {
 // -------------------------------------------------
 //	util
 // -------------------------------------------------
-
-//
+mz.util = {};
 //
 mz.util.typeof = function(o,t){
 	return _mz_.toString(o).indexOf(t) > 0;
 };
 
 //
-//
-mz.core.loop(
+mz.core.each(
 	["Object","String","Number","Array","Boolean"],
 	function(data){
 		mz.util["is"+data] = function(t) {
@@ -73,8 +93,8 @@ mz.core.loop(
 // -------------------------------------------------
 //	document
 // -------------------------------------------------
+mz.doc = {};
 
-//
 //
 mz.doc.get = function(s) {
 	var doc = _mz_.document();
@@ -87,36 +107,11 @@ mz.doc.get = function(s) {
 	return null;
 };
 
-
 // -------------------------------------------------
 //	alias
 // -------------------------------------------------
-mz.core.loop(
-	mz.core,
-	function(o,k){
-		mz[k] = o;
-	},
-	mz
-);
+mz.core.each(mz.core, function(v,k){ mz[k] = v; }, mz);
 
-// -------------------------------------------------
-//	private
-//	namespace : '_mz_'
-// -------------------------------------------------
-var _mz_ 	= _mz_ || {};
-_mz_.toString = function(t) {
-	return Object.prototype.toString.call(t);
-};
-
-_mz_.now = function(fmt) {
-	var dt 	= new Date();
-	fmt 		= fmt || "YYYY/MM/DD HH:MI:SS:MS";
-	return fmt.replace("YYYY",dt.getFullYear()).replace("YY", dt.getFullYear().toString().substring(2)).replace("MM", (dt.getMonth() + 1)).replace("DD", dt.getDate()).replace("HH", dt.getHours()).replace("MI", dt.getMinutes()).replace("SS", dt.getSeconds()).replace("MS", dt.getMilliseconds());
-};
-
-_mz_.document = function() {
-	return _mz_.doc = _mz_.doc || (window ? window.document : undefined);
-};
-
+root.mz = mz;
 
 }).call(this);
